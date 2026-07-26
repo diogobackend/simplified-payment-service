05a5d537c5c86654a0ac1588b610511dcb9f7f75
idd_dsferreira <idd_dsferreira@uolinc.com>
2026-07-26 17:03:58 -0300

    feat: add money value object

---
+++ src/main/kotlin/com/simplifiedpayment/core/common/messages/PaymentMessages.kt
@@ -0,0 +1,5 @@
+package com.simplifiedpayment.core.common.messages
+
+object PaymentMessages {
+    const val MONEY_MUST_NOT_BE_NEGATIVE = "Money must not be negative"
+}
---
+++ src/main/kotlin/com/simplifiedpayment/core/domain/valueobject/Money.kt
@@ -0,0 +1,26 @@
+package com.simplifiedpayment.core.domain.valueobject
+
+import com.simplifiedpayment.core.common.messages.PaymentMessages.MONEY_MUST_NOT_BE_NEGATIVE
+import java.math.BigDecimal
+
+data class Money(
+    val value: BigDecimal,
+) {
+    init {
+        require(value >= BigDecimal.ZERO) {
+            MONEY_MUST_NOT_BE_NEGATIVE
+        }
+    }
+
+    operator fun plus(money: Money): Money = Money(value.add(money.value))
+
+    operator fun minus(money: Money): Money = Money(value.subtract(money.value))
+
+    fun isLessThan(money: Money): Boolean = value < money.value
+
+    fun isGreaterThanOrEqualTo(money: Money): Boolean = value >= money.value
+
+    companion object {
+        val ZERO = Money(BigDecimal.ZERO)
+    }
+}
